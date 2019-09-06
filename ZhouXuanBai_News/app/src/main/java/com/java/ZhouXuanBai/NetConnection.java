7ff7141b1e08bc8ada729943eb52676a5918868d
zhouxb17 <zhouxb17@mails.tsinghua.edu.cn>
2019-09-06 21:08:43 +0800

    添加默认版本的网络连接请求

--- ZhouXuanBai_News/app/src/main/java/com/java/ZhouXuanBai/NetConnection.java
+++ ZhouXuanBai_News/app/src/main/java/com/java/ZhouXuanBai/NetConnection.java
@@ -3,27 +3,42 @@ package com.java.ZhouXuanBai;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
 
 public class NetConnection {
-
     public static String httpRequest(String params) {
-//        StringBuilder sb = new StringBuilder();
-//        for (Map.Entry i : params.entrySet()) {
-//            try {
-//                sb.append(i.getKey()).append("=").append(URLEncoder.encode(i.getValue() + "", "UTF-8")).append("&");
-//            } catch (UnsupportedEncodingException e) {
-//                e.printStackTrace();
-//            }
-//        }
         StringBuffer buffer = new StringBuffer();
         try {
             URL url = new URL("https://api2.newsminer.net/svc/news/queryNewsList?" + params);
+            HttpURLConnection httpUrlConn = (HttpURLConnection) url.openConnection();
+            httpUrlConn.setDoInput(true);
+            httpUrlConn.setRequestMethod("GET");
+            httpUrlConn.connect();
+
+            InputStream inputStream = httpUrlConn.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String str = null;
+            while ((str = bufferedReader.readLine()) != null) {
+                buffer.append(str);
+            }
+            bufferedReader.close();
+            inputStreamReader.close();
+            inputStream.close();
+            httpUrlConn.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return buffer.toString();
+    }
+    public static String httpRequest() {
+        StringBuffer buffer = new StringBuffer();
+        try {
+            URL url = new URL("https://api2.newsminer.net/svc/news/queryNewsList?size=15&startDate=2019-07-01&endDate=2019-07-03&words=特朗普&categories=科技");
             HttpURLConnection httpUrlConn = (HttpURLConnection) url.openConnection();
             httpUrlConn.setDoInput(true);
             httpUrlConn.setRequestMethod("GET");
